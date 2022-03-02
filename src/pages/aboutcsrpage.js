5d94d5bfd01bde4427791bbfd209f27b231b7a96
shivanshu32 <shivanshu32@gmail.com>
2022-03-02 11:52:38 +0530

    first commit

--- src/pages/aboutcsrpage.js
+++ src/pages/aboutcsrpage.js
@@ -7,6 +7,216 @@ class aboutcsrpage extends Component {
         return (
             <div>
                 <Navigation />
+
+
+                <div class="w-full bg-center bg-cover h-96" style={{backgroundImage: "url(images/about-banner.jpg)"}}>
+      <div class="flex items-center justify-left w-full h-full bg-gray-900 bg-opacity-50">
+          <div class="px-16">
+              <h1 class="text-2xl font-semibold text-white lg:text-4xl">To create a stronger business we <br />need a progressive society and a <br />sustainable development</h1>
+              
+          </div>
+      </div>
+  </div>
+
+
+  <h2 class="mb-6 font-sans py-6 text-3xl font-bold tracking-tight text-center text-black sm:text-4xl sm:leading-none">
+        About CSR    
+        </h2> 
+
+<section class="bg-white dark:bg-gray-800">
+  <div class="container px-16 mx-auto">
+      <div class="items-start lg:flex">
+       
+
+          <div class="lg:w-full">
+              
+              <p class="text-gray-500 dark:text-gray-400 ">
+                CSR initiatives at Effectual can be summarized in two words: Measurable Change. Our initiatives are focused on three key aspects of the society – Economic, Social, and Environmental.
+</p><p class="mt-4 text-gray-500 dark:text-gray-400 ">
+                From making our workplace more environmentally friendly to enabling clients discover solutions to some of the biggest ecological problems mankind is facing, we believe in doing our bit for our immediate environment and the world.
+              </p><p class="mt-4 text-gray-500 dark:text-gray-400 ">              
+                Our employees are also engaged directly in helping us achieve our CSR goals as a company.
+              </p>
+
+              <button class="w-full px-4 py-2 mt-4 text-sm font-medium text-white uppercase transition-colors duration-200 transform bg-blue-600 rounded-md lg:w-auto hover:bg-blue-500 uppercase focus:outline-none focus:bg-blue-500">Work with us</button>
+          </div>
+
+          
+      </div>
+  </div>
+</section>
+
+
+<section class="container p-6 mx-auto bg-white dark:bg-gray-800">
+  <h2 class="mb-6 font-sans text-3xl font-bold tracking-tight text-center text-black sm:text-4xl sm:leading-none">
+    CSR Focus Area     
+     </h2> 
+
+  <div class="flex items-center justify-center">
+      <div class="grid gap-0 mt-8 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5">
+          <div class="w-full max-w-xs text-center">
+              <img class="object-cover object-center w-full mx-auto " src="images/about-block1.jpg" alt="avatar"/>
+
+              <div class="mt-2">
+                  <h3 class="text-lg font-medium text-gray-700 dark:text-gray-200">GEO EFFECT
+                    </h3>
+                  <span class="mt-1 font-medium text-gray-600 dark:text-gray-300">(TREE PLANTATION DRIVE)</span>
+              </div>
+          </div>
+
+          <div class="w-full max-w-xs text-center">
+              <img class="object-cover object-center w-full mx-auto" src="images/about-block2.jpg" alt="avatar"/>
+
+              <div class="mt-2">
+                  <h3 class="text-lg font-medium text-gray-700 dark:text-gray-200">Mental Health</h3>
+                  
+              </div>
+          </div>
+
+          <div class="w-full max-w-xs text-center">
+              <img class="object-cover object-center w-full mx-auto" src="images/national.jpg" alt="avatar"/>
+
+              <div class="mt-2">
+                  <h3 class="text-lg font-medium text-gray-700 dark:text-gray-200">National Give Away Day</h3>
+                  
+              </div>
+          </div>
+
+          <div class="w-full max-w-xs text-center">
+              <img class="object-cover object-center w-full mx-auto" src="images/breast-can-2.jpg" alt="avatar"/>
+
+              <div class="mt-2">
+                  <h3 class="text-lg font-medium text-gray-700 dark:text-gray-200">Breast Cancer</h3>
+                  <span class="mt-1 font-medium text-gray-600 dark:text-gray-300">AWARENESS DRIVE</span>
+                
+              </div>
+          </div>
+
+          <div class="w-full max-w-xs text-center">
+            <img class="object-cover object-center w-full mx-auto" src="images/about-block5.jpg" alt="avatar"/>
+
+            <div class="mt-2">
+                <h3 class="text-lg font-medium text-gray-700 dark:text-gray-200">AIDS</h3>
+                <span class="mt-1 font-medium text-gray-600 dark:text-gray-300">AWARENESS DRIVE</span>
+            </div>
+        </div>
+      </div>
+  </div>
+</section>
+
+
+<h2 class="mb-6 font-sans text-3xl font-bold tracking-tight text-center text-black sm:text-4xl sm:leading-none">
+  CSR Activities   
+   </h2> 
+
+
+<section class="bg-white dark:bg-gray-800">
+  <div class="container px-16 py-8 mx-auto">
+      <div class="items-start lg:flex">
+        <div class="mt-8 lg:mt-0 lg:w-1/4">
+          <div class="flex items-center justify-left">
+              <div class="max-w-lg">
+                  <img class="object-cover object-center w-24 h-24 rounded-md shadow" src="images/geo-effect-icon.png" alt="geoeffect" />
+              </div>              
+          </div>          
+      </div>
+          <div class="lg:w-auto">
+              <h2 class="text-3xl font-bold text-gray-800 dark:text-gray-100">Geo-Effect (Tree Plantation Drive)</h2>
+              <p class="mt-4 text-gray-500 dark:text-gray-400 ">
+                Under our ‘Geo-Effect’ initiative, we have come up with a Tree plantation drive. We have a mission to plant at least 5,000 trees across Delhi-NCR in 2021. For this, our team is engaged with different NGOs to help us achieve our goal.
+</p><p class="mt-4 text-gray-500 dark:text-gray-400 ">
+                Our employees have also volunteered to help with our mission. Since we all are working from home, we have created some green activities that they can do with their families. We have created these activities in such a way that they can utilize the waste material around them and use them to plant trees.
+              </p>              
+          </div>          
+      </div>
+  </div>
+</section>
+
+   <section class="bg-white dark:bg-gray-800">
+    <div class="container px-16 py-8 mx-auto">
+        <div class="items-start lg:flex">
+          <div class="mt-8 lg:mt-0 lg:w-1/4">
+            <div class="flex items-center justify-left">
+                <div class="max-w-lg">
+                    <img class="object-cover object-center w-24 h-24 rounded-md shadow" src="images/mental-health-icon.png" alt="healthicon" />
+                </div>              
+            </div>          
+        </div>
+            <div class="lg:w-auto">
+                <h2 class="text-3xl font-bold text-gray-800 dark:text-gray-100">Mental Health</h2>
+                <p class="mt-4 text-gray-500 dark:text-gray-400 ">
+                  The pandemic and the subsequent lockdown that followed, has impacted the mental health of many. We have initiated programs to help our employees manage stress.
+                </p><p class="mt-4 text-gray-500 dark:text-gray-400 ">
+                  We have planned different awareness and training programs on Stress Management. These programs will help our colleagues identify their stress level and train them to deal with workload, isolation, stress, anxiety etc. We have also planned virtual sessions focused on yoga and meditation that can help them strengthen and relax their mind.
+                </p>              
+            </div>          
+        </div>
+    </div>
+  </section>
+
+<section class="bg-white dark:bg-gray-800">
+  <div class="container px-16 py-8 mx-auto">
+      <div class="items-start lg:flex">
+        <div class="mt-8 lg:mt-0 lg:w-1/4 px-6">
+          <div class="flex items-center justify-left">
+              <div class="max-w-lg">
+                  <img class="object-cover object-center w-24 h-24 rounded-md shadow" src="images/national-give-away-day-icon.png" alt="giveaway" />
+              </div>              
+          </div>          
+      </div>
+          <div class="lg:w-auto">
+              <h2 class="text-3xl font-bold text-gray-800 dark:text-gray-100">National Give Away Day</h2>
+              <p class="mt-4 text-gray-500 dark:text-gray-400 ">
+                Through our tie-up with Children community centers, we have decided to be guardian angels to underprivileged children to celebrate National Give Away Day. Our agenda is to improve our community in a way that benefits all. We plan on taking this opportunity to spread joy by giving something they need. Showing empathy, encouragement, and compassion to someone during the time of need empowers us all.
+              </p>              
+          </div>          
+      </div>
+  </div>
+</section>
+
+   <section class="bg-white dark:bg-gray-800">
+    <div class="container px-16 py-8 mx-auto">
+        <div class="items-start lg:flex">
+          <div class="mt-8 lg:mt-0 lg:w-1/4">
+            <div class="flex items-center justify-left">
+                <div class="max-w-lg">
+                    <img class="object-cover object-center w-24 h-24 rounded-md shadow" src="images/breast-cancer-icon.png" alt="breastcancer" />
+                </div>              
+            </div>          
+        </div>
+            <div class="lg:w-auto">
+                <h2 class="text-3xl font-bold text-gray-800 dark:text-gray-100">
+                  Breast Cancer Awareness Drive</h2>
+                <p class="mt-4 text-gray-500 dark:text-gray-400 ">
+                  We have initiated a Breast Cancer Awareness program that is designed to impart knowledge regarding the topic and inculcate safe lifestyle practices. Our program encourages our employees to teach others while they learn. At the end of the drive we have planned an event where our employees will impart wisdom through play, videos and songs etc. to generate awareness all around.
+                </p>              
+            </div>          
+        </div>
+    </div>
+  </section>
+
+<section class="bg-white dark:bg-gray-800">
+  <div class="container px-16 py-8 mx-auto">
+      <div class="items-start lg:flex">
+        <div class="mt-8 lg:mt-0 lg:w-1/4">
+          <div class="flex items-center justify-left">
+              <div class="max-w-lg">
+                  <img class="object-cover object-center w-24 h-24 rounded-md shadow" src="images/aids-awareness.jpg" alt="aidsawareness" />
+              </div>              
+          </div>          
+      </div>
+          <div class="lg:w-auto">
+              <h2 class="text-3xl font-bold text-gray-800 dark:text-gray-100">Aids Awareness Drive</h2>
+              <p class="mt-4 text-gray-500 dark:text-gray-400 ">
+                The program is designed to offer support for those living with the disease, education for those trying to prevent transmission of the disease, and remembrance for those who have lost their battle to HIV AIDS. We are fighting to prevent AIDS, educating people on treatment and its transmission, stigma and discrimination.
+              </p>              
+          </div>          
+      </div>
+  </div>
+</section>
+
+
+
                 <Footer />
             </div>
         );
