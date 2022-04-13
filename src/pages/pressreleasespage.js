ae19f91657dfaaa5c64345aa4bdc285e5ec7263a
shivanshu32 <shivanshu32@gmail.com>
2022-04-13 15:40:04 +0530

    press release

--- src/pages/pressreleasespage.js
+++ src/pages/pressreleasespage.js
@@ -28,115 +28,53 @@ class pressreleasespage extends Component {
           <img class="h-56 w-full  sm:h-64 md:h-96 lg:w-full lg:h-full object-contain" src="images/banner-press-release.png" alt="" />
         </div>
       </div>
-            
-<section class="text-gray-600 body-font">
-  <div class="container px-5 py-24 mx-auto">
-    <div class="flex flex-wrap -mx-4 -mb-10 text-center">
-      <div class="sm:w-1/2 mb-10 px-4">
-        
-        <h2 class="title-font text-2xl font-medium text-gray-900 mt-6 mb-3">Effectual Services is proud to announce that Dr. Amit Goel is recognized as IAM Strategy 300 Global Leader</h2>
-        <p class="leading-relaxed text-base">19th January 2021, Noida- Dr. Amit Goel has been recognized as one of the Global Leaders by IAM Strategy 300. Dr. Amit Goel, Director has been handling client engagements with tech firms, law firms and universities, for providing patent prosecution, patent commercialization, patent litigation, and patent analytic services. He has expertise in patent litigation and patent enforcement</p>
-        <button class="flex mx-auto mt-6 text-white bg-indigo-500 border-0 py-2 px-5 focus:outline-none hover:bg-indigo-600 rounded">Read More</button>
-      </div>
-      <div class="sm:w-1/2 mb-10 px-4">
-        
-        <h2 class="title-font text-2xl font-medium text-gray-900 mt-6 mb-3">INDIA 500 MOST PROMISING INTELLECTUAL PROPERTY LEADER AWARD 2020 winner of the year</h2>
-        <p class="leading-relaxed text-base">Amit Goel Takes India 500 Most Promising Intellectual Property Leader 2020 Award 3rd September 2020, Noida- Amit Goel is a Leader Award winner in the 2020 India 500 Most Promising Intellectual Property Leader Award competition for Quality Excellence. Amit Goel has an overall experience of 18+ years in IP industry and is co-founder and director of Effectual Services. Effectual Services</p>
-        <button class="flex mx-auto mt-6 text-white bg-indigo-500 border-0 py-2 px-5 focus:outline-none hover:bg-indigo-600 rounded">Read More</button>
-      </div>
-    </div>
-  </div>
 
-  <div class="container px-5 mx-auto">
-    <div class="flex flex-wrap -mx-4 -mb-10 text-center">
-      <div class="sm:w-1/2 mb-10 px-4">
-        
-        <h2 class="title-font text-2xl font-medium text-gray-900 mt-6 mb-3">Effectual Services appoints Ms. Meetika Aggarwal as the New Director of the Company</h2>
-        <p class="leading-relaxed text-base">The Met cognitive Business woman joins Effectual Services with Experience of 18+ years New Delhi, August 2020: Effectual Services, a leading Intellectual Property Management advisory firm announced the appointment of Ms. Meetika Aggarwal as the Director for the company. Ms. Aggarwal will spearhead the strategic planning, business transitions to accelerate the growth of the company. Ms.</p>
-        <button class="flex mx-auto mt-6 text-white bg-indigo-500 border-0 py-2 px-5 focus:outline-none hover:bg-indigo-600 rounded">Read More</button>
-      </div>
-      <div class="sm:w-1/2 mb-10 px-4">
-        
-        <h2 class="title-font text-2xl font-medium text-gray-900 mt-6 mb-3">Dr. Amit Goel Recognized in IAM Strategy 300: The World’s Leading IP Strategists Annual Listing</h2>
-        <p class="leading-relaxed text-base">Dr.Amit Goel has been recognized in the IAM Strategy 300: World’s Leading IP Strategists 2020, an annual listing of individuals who lead the way in the development and implementation of world-class programs that maximize the value of intellectual property portfolios. According to IAM Media, the strategists identified in this guide do not see issues from just one perspective; instead,</p>
-        <button class="flex mx-auto mt-6 text-white bg-indigo-500 border-0 py-2 px-5 focus:outline-none hover:bg-indigo-600 rounded">Read More</button>
-      </div>
-    </div>
-  </div>
+                  
 
-  <div class="container px-5 mx-auto">
-    <div class="flex flex-wrap -mx-4 -mb-10 text-center">
-      <div class="sm:w-1/2 mb-10 px-4">
-        
-        <h2 class="title-font text-2xl font-medium text-gray-900 mt-6 mb-3">Effectual Services announces sudden demise of Co-founder, Director- Effectual Services, Amit Aggarwal</h2>
-        <p class="leading-relaxed text-base">Noida, India: We at Effectual Services, with profound shock and sadness, announce the untimely demise of one of our co-founders and director, Mr. Amit Aggarwal on August 4th 2020. We extend our most heartfelt condolences and sympathy to Amit’s family. Our thoughts and prayers are with them at this most difficult time. Amit was fighting an</p>
-        <button class="flex mx-auto mt-6 text-white bg-indigo-500 border-0 py-2 px-5 focus:outline-none hover:bg-indigo-600 rounded">Read More</button>
+      <div class="container py-6 mx-auto grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 pt-6 gap-8">                   
+                    <div class="rounded  h-96">
+                    <iframe class="w-full h-full" src="https://www.linkedin.com/embed/feed/update/urn:li:ugcPost:6918349051281235969"  frameborder="0" allowfullscreen="" title="Embedded post"></iframe>
+                    </div>                   
+                    <div class="rounded  h-96">
+                    <iframe class="w-full h-full" src="https://www.linkedin.com/embed/feed/update/urn:li:ugcPost:6916717588655857664" frameborder="0" allowfullscreen="" title="Embedded post"></iframe>
+                    </div>
       </div>
-      <div class="sm:w-1/2 mb-10 px-4">
-        
-        <h2 class="title-font text-2xl font-medium text-gray-900 mt-6 mb-3">IP consultancies mushroom in Mohali, Chandigarh</h2>
-        <p class="leading-relaxed text-base">The influx of research institutes, universities, manufacturing and IT companies in Chandigarh, Punjab and Himachal Pradesh has become a cynosure of Intellectual Property (IP) consultancy firms. There are over a dozen firms in public and private sector besides individual consultants in Chandigarh and Mohali which cater to businesses and technical institutions in their innovation, patents,</p>
-        <button class="flex mx-auto mt-6 text-white bg-indigo-500 border-0 py-2 px-5 focus:outline-none hover:bg-indigo-600 rounded">Read More</button>
+
+      <div class="container py-6 mx-auto grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 pt-6 gap-8">                   
+                    <div class="rounded  h-96">
+                    <iframe class="w-full h-full" src="https://www.linkedin.com/embed/feed/update/urn:li:ugcPost:6914450207103488000" frameborder="0" allowfullscreen="" title="Embedded post"></iframe>
+                    </div>                   
+                    <div class="rounded  h-96">
+                    <iframe class="w-full h-full" src="https://www.linkedin.com/embed/feed/update/urn:li:ugcPost:6911930709259546624" frameborder="0" allowfullscreen="" title="Embedded post"></iframe>
+                    </div>
       </div>
-    </div>
-  </div>
 
-  <div class="container px-5 mx-auto">
-    <div class="flex flex-wrap -mx-4 -mb-10 text-center">
-      <div class="sm:w-1/2 mb-10 px-4">
-        
-        <h2 class="title-font text-2xl font-medium text-gray-900 mt-6 mb-3">पंजाब के मोहाली में इफेक्चुअल सर्विसेज ने नया ऑफिस खोलने की घोषणा की</h2>
-        <p class="leading-relaxed text-base">एक प्रमुख इंटेलेक्चुअल प्रॉपर्टी मैनेजमेंट एडवाइजरी फर्म, इफेक्चुअल सर्विसेज, ने भारत में अपनी मौजूदगी को और दमदार बनाने और बिजनेस और कंपनी का दायरा फैलाने की योजना बनाई है। इफेक्चुअल सर्विसेज ने मोहाली में नया ऑफिस खोलने की घोषणा की है। मोहाली स्थित ऑफिस में 100 से ज्यादा कर्मचारियों की भर्ती की जाएगी। 2019 में</p>
-        <button class="flex mx-auto mt-6 text-white bg-indigo-500 border-0 py-2 px-5 focus:outline-none hover:bg-indigo-600 rounded">Read More</button>
+      <div class="container py-6 mx-auto grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 pt-6 gap-8">                   
+                    <div class="rounded  h-96">
+                    <iframe class="w-full h-full" src="https://www.linkedin.com/embed/feed/update/urn:li:share:6915236491577233408" frameborder="0" allowfullscreen="" title="Embedded post"></iframe>
+                    </div>                   
+                    <div class="rounded  h-96">
+                    <iframe class="w-full h-full" src="https://www.linkedin.com/embed/feed/update/urn:li:share:6916999735723593728"  frameborder="0" allowfullscreen="" title="Embedded post"></iframe>
+                    </div>
       </div>
-      <div class="sm:w-1/2 mb-10 px-4">
-        
-        <h2 class="title-font text-2xl font-medium text-gray-900 mt-6 mb-3">Effectual Services announces the opening of a new office in Mohali, Punjab</h2>
-        <p class="leading-relaxed text-base">With plans to expand company’s business,The new office in Mohali will have a total headcount of 100 employees by end of the year 2020 Mohali, July 2020: Effectual Services, a leading Intellectual Property Management advisory firm continues to strengthen its presence in India and plans to expand business and company footprint. Effectual Services announced the opening</p>
-        <button class="flex mx-auto mt-6 text-white bg-indigo-500 border-0 py-2 px-5 focus:outline-none hover:bg-indigo-600 rounded">Read More</button>
+
+      <div class="container py-6 mx-auto grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 pt-6 gap-8">                   
+                    <div class="rounded  h-96">
+                    <iframe class="w-full h-full" src="https://www.linkedin.com/embed/feed/update/urn:li:share:6905548608289271808" frameborder="0" allowfullscreen="" title="Embedded post"></iframe>
+                    </div>                   
+                    <div class="rounded  h-96">
+                    <iframe class="w-full h-full" src="https://www.linkedin.com/embed/feed/update/urn:li:ugcPost:6906536769676824576" frameborder="0" allowfullscreen="" title="Embedded post"></iframe>
+                    </div>
       </div>
-    </div>
-  </div>
 
-  <div class="container px-5 mx-auto">
-    <div class="flex flex-wrap -mx-4 -mb-10 text-center">
-      <div class="sm:w-1/2 mb-10 px-4">
-        
-        <h2 class="title-font text-2xl font-medium text-gray-900 mt-6 mb-3">Tech tools to draft patent applications could revolutionise IP proceedings</h2>
-        <p class="leading-relaxed text-base">Highly skilled patent professionals carry out the predominantly manual task of drafting a patent application, which involves the use of set phrases and a substantial amount of copying and pasting. The smallest of mistakes in the drafted application or claims can lead to serious problems further down the line. However, due to the repetitive nature</p>
-        <button class="flex mx-auto mt-6 text-white bg-indigo-500 border-0 py-2 px-5 focus:outline-none hover:bg-indigo-600 rounded">Read More</button>
+      <div class="container py-6 mx-auto grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 pt-6 gap-8">                   
+                    <div class="rounded  h-96">
+                    <iframe class="w-full h-full" src="https://www.linkedin.com/embed/feed/update/urn:li:ugcPost:6906862411043733504"  frameborder="0" allowfullscreen="" title="Embedded post"></iframe>
+                    </div>                   
+                    <div class="rounded  h-96">
+                    <iframe class="w-full h-full" src="https://www.linkedin.com/embed/feed/update/urn:li:share:6910180723119316993" frameborder="0" allowfullscreen="" title="Embedded post"></iframe>
+                    </div>
       </div>
-      <div class="sm:w-1/2 mb-10 px-4">
-        
-        <h2 class="title-font text-2xl font-medium text-gray-900 mt-6 mb-3">Blockchain and intellectual property – the de-centralised alliance</h2>
-        <p class="leading-relaxed text-base">Blockchain is an anonymous online ledger that simplifies the way in which transactions are carried out. It comprises a growing list of records, called blocks, which are linked using cryptography. Each block contains a cryptographic hash of the previous one, a timestamp and transaction data. The technology was invented in 2008 in the form of</p>
-        <button class="flex mx-auto mt-6 text-white bg-indigo-500 border-0 py-2 px-5 focus:outline-none hover:bg-indigo-600 rounded">Read More</button>
-      </div>
-    </div>
-  </div>
-
-</section>
-<div class="flex justify-center w-full py-16">
-  <a href="/" class="flex items-center px-4 py-2 mx-1 text-gray-500 bg-white rounded-md cursor-not-allowed dark:bg-gray-800 dark:text-gray-600">
-      previous
-  </a>
-
-  <a href="/" class="flex items-center px-4 py-2 mx-1 text-gray-700 transition-colors duration-200 transform bg-white rounded-md dark:bg-gray-800 dark:text-gray-200 hover:bg-blue-600 dark:hover:bg-blue-500 hover:text-white dark:hover:text-gray-200">
-      1
-  </a>
-
-  <a href="/" class="flex items-center px-4 py-2 mx-1 text-gray-700 transition-colors duration-200 transform bg-white rounded-md dark:bg-gray-800 dark:text-gray-200 hover:bg-blue-600 dark:hover:bg-blue-500 hover:text-white dark:hover:text-gray-200">
-      2
-  </a>
-
-  <a href="/" class="flex items-center px-4 py-2 mx-1 text-gray-700 transition-colors duration-200 transform bg-white rounded-md dark:bg-gray-800 dark:text-gray-200 hover:bg-blue-600 dark:hover:bg-blue-500 hover:text-white dark:hover:text-gray-200">
-      3
-  </a>
-
-  <a href="/" class="flex items-center px-4 py-2 mx-1 text-gray-700 transition-colors duration-200 transform bg-white rounded-md dark:bg-gray-800 dark:text-gray-200 hover:bg-blue-600 dark:hover:bg-blue-500 hover:text-white dark:hover:text-gray-200">
-      Next
-  </a>
-</div>
 
             
 
